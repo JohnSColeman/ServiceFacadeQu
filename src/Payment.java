c794c2362aa7ddb8968cb310543e468e474a75e7
John Coleman <qbyteconsulting@gmail.com>
2020-05-26 05:06:49 +0700

    throw IOException up so it's not lost

--- src/Payment.java
+++ src/Payment.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,8 +16,8 @@ public class Payment {
         this.database = database;
     }
 
-    /* If the connection/database call fails pass up the exception, if the gateway fails rollback. */
-    public void charge(Double amount, String description)  throws SQLException {
+    /* If the connection/database call fails pass up the exception, if the gateway fails rollback and throw. */
+    public void charge(Double amount, String description)  throws SQLException, IOException {
         Connection con = factory.getConnection();
         try {
             database.charge(con, amount, description);
@@ -26,6 +28,7 @@ public class Payment {
             }
         } catch (IOException ioex) {
             con.rollback();
+            throw ioex;
         } finally {
             con.close();
         }
